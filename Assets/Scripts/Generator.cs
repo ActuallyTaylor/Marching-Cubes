07ae11e8ec74d4e3bb199902b0c2da01f046c7c4
Zachary Lineman <zachary.lineman@gmail.com>
2020-04-07 17:32:40 -0400

    Some changes
    Still working on getting chunk generation to work. Things just arent lining up correctly. Can't figure out why

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Generator : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class Generator : MonoBehaviour
     public Vector3 offset;
     [Range(0, 1)]
     public float threshold;
+    bool created = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,24 +31,66 @@ public class Generator : MonoBehaviour
 
     void createMesh()
     {
-        float[,,] map3D = Noise.Generate3DNoiseMap(chunkSize, seed, scale, octaves, persistance, lacunarity, offset);
+        float[,,] map = Noise.Generate3DNoiseMap(chunkSize, seed, scale, octaves, persistance, lacunarity, offset + transform.position);
 
+        List<CombineInstance> blockData = new List<CombineInstance>();
+        MeshFilter blockMesh = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), Vector3.zero, Quaternion.identity).GetComponent<MeshFilter>();
         for (int x = 0; x < chunkSize; x++)
         {
             for(int z = 0; z < chunkSize; z++)
             {
                 for (int y = 0; y < chunkSize; y++)
                 {
-                    float densityValue = map3D[x, y, z];
+                    float densityValue = map[x, y, z];
                     if (densityValue >= threshold)
                     {
-                        GameObject block = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        block.transform.localScale = new Vector3(1f, 1f, 1f);
-                        block.transform.parent = transform;
-                        block.transform.position = new Vector3(x + transform.position.x, y + transform.position.y, z + transform.position.z);
+                        blockMesh.transform.position = new Vector3(x + transform.position.x, y + transform.position.y, z + transform.position.z);
+                        CombineInstance ci = new CombineInstance
+                        {
+                            mesh = blockMesh.sharedMesh,
+                            transform = blockMesh.transform.localToWorldMatrix,
+                        };
+                        blockData.Add(ci);
                     }
                 }
             }
+        }
+        Destroy(blockMesh.gameObject);
+
+        List<List<CombineInstance>> blockDataLists = new List<List<CombineInstance>>();
+        int vertexCount = 0;
+        blockDataLists.Add(new List<CombineInstance>());
+        for (int i = 0; i < blockData.Count; i++)
+        {
+            vertexCount += blockData[i].mesh.vertexCount;
+            if (vertexCount > 65536)
+            {
+                vertexCount = 0;
+                blockDataLists.Add(new List<CombineInstance>());
+                i--;
+            }
+            else
+            {
+                blockDataLists.Last().Add(blockData[i]);
+            }
+        }
+
+        Transform container = new GameObject("World").transform;
+        foreach (List<CombineInstance> data in blockDataLists)
+        {
+            GameObject g = new GameObject("Chunk");
+            g.transform.parent = container;
+            MeshFilter mf = g.AddComponent<MeshFilter>();
+            MeshRenderer mr = g.AddComponent<MeshRenderer>();
+            mr.material = blockMesh.GetComponent<MeshRenderer>().material;
+            mf.mesh.CombineMeshes(data.ToArray());
+            //g.AddComponent<MeshCollider>().sharedMesh = mf.sharedMesh;
+        }
+        transform.position = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(chunkSize, 0, 0), 10000 * Time.deltaTime);
+        if(created == false)
+        {
+            created = true;
+            createMesh();
         }
     }
 }
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    public GameObject blockPrefab;
+    public int chunkSize = 50;
+    public float noiseScale = .05f;
+    [Range(0, 1)]
+    public float threshold = .5f;
+    public Material material;
+    public bool sphere = false;
+    public int seed;
+    private List<Mesh> meshes = new List<Mesh>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Destroy(GameObject.Find("World"));
+            foreach (Mesh m in meshes)
+                Destroy(m);
+            Generate();
+        }
+    }
+
+    public void Start()
+    {
+        Destroy(GameObject.Find("World"));
+        foreach (Mesh m in meshes)
+            Destroy(m);
+        Generate();
+    }
+
+    private void Generate()
+    {
+        List<CombineInstance> blockData = new List<CombineInstance>();
+        MeshFilter blockMesh = Instantiate(blockPrefab, Vector3.zero, Quaternion.identity).GetComponent<MeshFilter>();
+
+        System.Random prng = new System.Random(seed);
+        float offsetX = prng.Next(-100000, 100000);
+        float offsetY = prng.Next(-100000, 100000);
+        float offsetZ = prng.Next(-100000, 100000);
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+
+                    float noiseValue = Perlin3D(x * noiseScale + offsetX, y * noiseScale + offsetY, z * noiseScale + offsetZ);
+                    if (noiseValue >= threshold)
+                    {
+                        float raduis = chunkSize / 2;
+                        if (sphere && Vector3.Distance(new Vector3(x, y, z), Vector3.one * raduis) > raduis)
+                            continue;
+
+                        blockMesh.transform.position = new Vector3(x, y, z);
+                        CombineInstance ci = new CombineInstance
+                        {
+                            mesh = blockMesh.sharedMesh,
+                            transform = blockMesh.transform.localToWorldMatrix,
+                        };
+                        blockData.Add(ci);
+                    }
+
+                }
+            }
+        }
+
+        Destroy(blockMesh.gameObject);
+
+        List<List<CombineInstance>> blockDataLists = new List<List<CombineInstance>>();
+        int vertexCount = 0;
+        blockDataLists.Add(new List<CombineInstance>());
+        for (int i = 0; i < blockData.Count; i++)
+        {
+            vertexCount += blockData[i].mesh.vertexCount;
+            if (vertexCount > 65536)
+            {
+                vertexCount = 0;
+                blockDataLists.Add(new List<CombineInstance>());
+                i--;
+            }
+            else
+            {
+                blockDataLists.Last().Add(blockData[i]);
+            }
+        }
+
+        Transform container = new GameObject("World").transform;
+        foreach (List<CombineInstance> data in blockDataLists)
+        {
+            GameObject g = new GameObject("Chunk");
+            g.transform.parent = container;
+            MeshFilter mf = g.AddComponent<MeshFilter>();
+            MeshRenderer mr = g.AddComponent<MeshRenderer>();
+            mr.material = material;
+            mf.mesh.CombineMeshes(data.ToArray());
+            meshes.Add(mf.mesh);
+            //g.AddComponent<MeshCollider>().sharedMesh = mf.sharedMesh;
+        }
+
+    }
+
+    public static float Perlin3D(float x, float y, float z)
+    {
+        float ab = Mathf.PerlinNoise(x, y);
+        float bc = Mathf.PerlinNoise(y, z);
+        float ac = Mathf.PerlinNoise(x, z);
+
+        float ba = Mathf.PerlinNoise(y, x);
+        float cb = Mathf.PerlinNoise(z, y);
+        float ca = Mathf.PerlinNoise(z, x);
+
+        float abc = ab + bc + ac + ba + cb + ca;
+        return abc / 6f;
+    }
+
+}
