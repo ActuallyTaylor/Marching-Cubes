49583c5e14b42b1c0cee4a41acc6febf681525e4
Zachary Lineman <zachary.lineman@gmail.com>
2020-04-08 17:06:27 -0400

    Some mesh enhancments
    Renders less vertices depending on where a cube is made

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,7 +5,8 @@ using System.Linq;
 
 public class Generator : MonoBehaviour
 {
-    public enum NoiseEnum { Noise3D, Noise2D };
+    public enum DimensionEnum { Noise3D, Noise2D };
+    public enum NoiseTypes {Value, ValueFractal, Perlin, PerlinFractal, Simplex, SimplexFractal, Cellular, WhiteNoise, Cubic, CubicFractal };
 
     public Vector3 chunkSize;
     public Vector3 regionSize;
@@ -15,7 +16,8 @@ public class Generator : MonoBehaviour
     public float threshold;
     public Material material;
     public bool drawDebugSqaures;
-    public NoiseEnum NType;
+    public DimensionEnum DimensionType;
+    public NoiseTypes NoiseType;
     public int scale2D;
 
     int chunkWidth;
@@ -38,10 +40,10 @@ public class Generator : MonoBehaviour
                 for (int k = 1; k <= regionSize.z; k++)
                 {
                     position = new Vector3(chunkWidth * j, chunkHeight * i, chunkDepth * k);
-                    if(NType == NoiseEnum.Noise3D)
+                    if(DimensionType == DimensionEnum.Noise3D)
                     {
                         load3DMesh();
-                    } else if(NType == NoiseEnum.Noise2D)
+                    } else if(DimensionType == DimensionEnum.Noise2D)
                     {
                         load2Dmesh();
                     }
@@ -56,85 +58,10 @@ public class Generator : MonoBehaviour
 
     }
 
-    void load2Dmesh()
-    {
-        FastNoise noise2D = new FastNoise();
-        noise2D.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
-        noise2D.SetSeed(seed);
-
-        float[,] heightMap = new float[chunkWidth, chunkDepth];
-
-        List<CombineInstance> blockData = new List<CombineInstance>();
-        for (int x = 0; x < chunkWidth; x++)
-        {
-            for (int y = 0; y < chunkDepth; y++)
-            {
-                heightMap[x, y] = noise2D.GetNoise(x + position.x + offset.x, y + position.z + offset.y);
-            }
-        }
-
-        for (int x = 0; x < chunkWidth; x++)
-        {
-            for (int y = 0; y < chunkDepth; y++)
-            {
-                Cube cube = new Cube();
-                cube.drawFullCube();
-
-                GameObject gCube = new GameObject("SubCube");
-                cube.calculateTriangles();
-                MeshFilter meshFilter = gCube.AddComponent<MeshFilter>();
-                MeshRenderer mr = gCube.AddComponent<MeshRenderer>();
-                float heightValue = heightMap[x, y] * scale2D;
-
-                gCube.transform.position = new Vector3(x + position.x, heightValue ,y + position.z);
-
-                meshFilter.mesh.vertices = cube.getVerts().ToArray();
-                meshFilter.mesh.triangles = cube.getTriangles().ToArray();
-                CombineInstance ci = new CombineInstance
-                {
-                    mesh = gCube.GetComponent<MeshFilter>().mesh,
-                    transform = gCube.transform.localToWorldMatrix,
-                };
-                blockData.Add(ci);
-                Destroy(gCube);
-            }
-        }
-
-        List<List<CombineInstance>> blockDataLists = new List<List<CombineInstance>>();
-        int vertexCount = 0;
-        blockDataLists.Add(new List<CombineInstance>());
-        for (int i = 0; i < blockData.Count; i++)
-        {
-            vertexCount += blockData[i].mesh.vertexCount;
-            if (vertexCount > 65536)
-            {
-                vertexCount = 0;
-                blockDataLists.Add(new List<CombineInstance>());
-                i--;
-            }
-            else
-            {
-                blockDataLists.Last().Add(blockData[i]);
-            }
-        }
-
-        foreach (List<CombineInstance> data in blockDataLists)
-        {
-            GameObject g = new GameObject("Chunk");
-            g.transform.parent = transform;
-            MeshFilter mf = g.AddComponent<MeshFilter>();
-            MeshRenderer mr = g.AddComponent<MeshRenderer>();
-            mr.material = mr.material = material;
-            mf.mesh.CombineMeshes(data.ToArray());
-            mf.mesh.RecalculateNormals();
-            //g.AddComponent<MeshCollider>().sharedMesh = mf.sharedMesh;
-        }
-    }
-
     void load3DMesh()
     {
         FastNoise noise3D = new FastNoise();
-        noise3D.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
+        noise3D.SetNoiseType(getNoiseType());
         noise3D.SetSeed(seed);
         float[,,] heightMap = new float[chunkWidth, chunkHeight, chunkDepth];
 
@@ -162,35 +89,52 @@ public class Generator : MonoBehaviour
                     if (densityValue >= threshold)
                     {
                         Cube cube = new Cube();
-                        if(y == chunkHeight - 1 || x == chunkWidth - 1 || z == chunkDepth - 1 || x == 0 || y == 0 || z == 0)
-                        {
-                            cube.drawFullCube();
-                        } else
-                        {
                             if (y < chunkHeight - 1 && heightMap[x, y + 1, z] < threshold) //Top
                             {
                                 cube.drawTop();
+                            } else if(y == chunkHeight - 1)
+                            {
+                                cube.drawTop();
                             }
+
                             if (y > 0 && heightMap[x, y - 1, z] < threshold) //Bottom
                             {
                                 cube.drawBottom();
+                            } else if(y == 0)
+                            {
+                                cube.drawBottom();
                             }
+
                             if (z < chunkDepth - 1 && heightMap[x, y, z + 1] < threshold) //Back
                             {
                                 cube.drawBack();
+                            } else if(z == chunkDepth -1)
+                            {
+                                cube.drawBack();
                             }
+
                             if (z > 0 && heightMap[x, y, z - 1] < threshold) //Front
                             {
                                 cube.drawFront();
+                            } else if(z == 0)
+                            {
+                                cube.drawFront();
                             }
+
                             if (x < chunkWidth - 1 && heightMap[x + 1, y, z] < threshold) //Right
                             {
                                 cube.drawRight();
+                            } else if ( x == chunkWidth - 1)
+                            {
+                                cube.drawRight();
                             }
+
                             if (x > 0 && heightMap[x - 1, y, z] < threshold) //left
                             {
                                 cube.drawLeft();
-                            }
+                            } else if ( x == 0)
+                            {
+                                cube.drawLeft();
                         }
 
 
@@ -255,6 +199,129 @@ public class Generator : MonoBehaviour
             mf.mesh.CombineMeshes(data.ToArray());
             mf.mesh.RecalculateNormals();
             //g.AddComponent<MeshCollider>().sharedMesh = mf.sharedMesh;
+        }
+    }
+
+    void load2Dmesh()
+    {
+        FastNoise noise2D = new FastNoise();
+        noise2D.SetNoiseType(getNoiseType());
+        noise2D.SetSeed(seed);
+
+        float[,] heightMap = new float[chunkWidth, chunkDepth];
+
+        List<CombineInstance> blockData = new List<CombineInstance>();
+        for (int x = 0; x < chunkWidth; x++)
+        {
+            for (int y = 0; y < chunkDepth; y++)
+            {
+                heightMap[x, y] = noise2D.GetNoise(x + position.x + offset.x, y + position.z + offset.y);
+            }
+        }
+
+        for (int x = 0; x < chunkWidth; x++)
+        {
+            for (int y = 0; y < chunkDepth; y++)
+            {
+                Cube cube = new Cube();
+                cube.drawFullCube();
+
+                GameObject gCube = new GameObject("SubCube");
+                cube.calculateTriangles();
+                MeshFilter meshFilter = gCube.AddComponent<MeshFilter>();
+                MeshRenderer mr = gCube.AddComponent<MeshRenderer>();
+                float heightValue = heightMap[x, y] * scale2D;
+
+                gCube.transform.position = new Vector3(x + position.x, heightValue, y + position.z);
+
+                meshFilter.mesh.vertices = cube.getVerts().ToArray();
+                meshFilter.mesh.triangles = cube.getTriangles().ToArray();
+                CombineInstance ci = new CombineInstance
+                {
+                    mesh = gCube.GetComponent<MeshFilter>().mesh,
+                    transform = gCube.transform.localToWorldMatrix,
+                };
+                blockData.Add(ci);
+                Destroy(gCube);
+            }
+        }
+
+        List<List<CombineInstance>> blockDataLists = new List<List<CombineInstance>>();
+        int vertexCount = 0;
+        blockDataLists.Add(new List<CombineInstance>());
+        for (int i = 0; i < blockData.Count; i++)
+        {
+            vertexCount += blockData[i].mesh.vertexCount;
+            if (vertexCount > 65536)
+            {
+                vertexCount = 0;
+                blockDataLists.Add(new List<CombineInstance>());
+                i--;
+            }
+            else
+            {
+                blockDataLists.Last().Add(blockData[i]);
+            }
+        }
+
+        foreach (List<CombineInstance> data in blockDataLists)
+        {
+            GameObject g = new GameObject("Chunk");
+            g.transform.parent = transform;
+            MeshFilter mf = g.AddComponent<MeshFilter>();
+            MeshRenderer mr = g.AddComponent<MeshRenderer>();
+            mr.material = mr.material = material;
+            mf.mesh.CombineMeshes(data.ToArray());
+            mf.mesh.RecalculateNormals();
+            //g.AddComponent<MeshCollider>().sharedMesh = mf.sharedMesh;
+        }
+    }
+
+    FastNoise.NoiseType getNoiseType()
+    {
+        if (NoiseType == NoiseTypes.Cellular)
+        {
+            return FastNoise.NoiseType.Cellular;
+        }
+        else if (NoiseType == NoiseTypes.Cubic)
+        {
+            return FastNoise.NoiseType.Cubic;
+        }
+        else if (NoiseType == NoiseTypes.CubicFractal)
+        {
+            return FastNoise.NoiseType.CubicFractal;
+        }
+        else if (NoiseType == NoiseTypes.Perlin)
+        {
+            return FastNoise.NoiseType.Perlin;
+        }
+        else if (NoiseType == NoiseTypes.PerlinFractal)
+        {
+            return FastNoise.NoiseType.PerlinFractal;
+        }
+        else if (NoiseType == NoiseTypes.Simplex)
+        {
+            return FastNoise.NoiseType.Simplex;
+        }
+        else if (NoiseType == NoiseTypes.SimplexFractal)
+        {
+            return FastNoise.NoiseType.SimplexFractal;
+        }
+        else if (NoiseType == NoiseTypes.Value)
+        {
+            return FastNoise.NoiseType.Value;
+        }
+        else if (NoiseType == NoiseTypes.ValueFractal)
+        {
+            return FastNoise.NoiseType.ValueFractal;
+        }
+        else if (NoiseType == NoiseTypes.WhiteNoise)
+        {
+            return FastNoise.NoiseType.WhiteNoise;
+        }
+        else
+        {
+            return FastNoise.NoiseType.Perlin;
         }
     }
 }
